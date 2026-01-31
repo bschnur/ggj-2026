54de2ee494f116cb4facfd7d0f75c5465eb89960
Benjamin Schnur <benjamin.schnur@gmail.com>
2026-01-31 15:58:06 -0800

    Now scaling the custom cursor images based on the OS-level display scaling (e.g. 2x for my retina display). I.e., cursor sizes correctly on Mac.

--- main.gd
+++ main.gd
@@ -1,5 +1,8 @@
 extends Node
 
+var current_screen_scale := 1.0
+var os_screen_scale: float
+
 @onready var title_screen := %TitleScreen
 @onready var pause_menu := %PauseMenu
 
@@ -10,20 +13,40 @@ enum FilterColor {
 }
 
 @export var filter_cursor_textures: Dictionary[FilterColor, Texture2D]
+var _filter_cursor_images: Dictionary[FilterColor, Image]
 
 var filter_color := FilterColor.NONE:
 	set(value):
 		filter_color = value
-		var cursor_texture := filter_cursor_textures[filter_color]
+		#var cursor_texture := filter_cursor_textures[filter_color]
+		var cursor_img := _filter_cursor_images[filter_color]
 		var hotspot := Vector2.ZERO
-		if cursor_texture is Texture2D:
-			hotspot = cursor_texture.get_size() * 0.5
-		Input.set_custom_mouse_cursor(cursor_texture, Input.CURSOR_ARROW, hotspot)
+		if cursor_img is Image:
+			hotspot = cursor_img.get_size() * 0.5
+		Input.set_custom_mouse_cursor(cursor_img, Input.CURSOR_ARROW, hotspot)
 		RenderingServer.global_shader_parameter_set("mouse_filtering_color_id", filter_color)
 
 
 func _ready() -> void:
+	os_screen_scale = DisplayServer.screen_get_max_scale()
+	init_cursor_images()
+	scale_cursor_images()
 	filter_color = FilterColor.RED
+
+func init_cursor_images() -> void:
+	for key in filter_cursor_textures:
+		var t := filter_cursor_textures[key]
+		if t is Texture2D:
+			_filter_cursor_images[key] = t.get_image()
+
+func scale_cursor_images() -> void:
+	if current_screen_scale != os_screen_scale:
+		var scale_factor = current_screen_scale / os_screen_scale
+		for img in _filter_cursor_images.values():
+			if img is Image:
+				var new_size: Vector2 = img.get_size() * scale_factor
+				img.resize(new_size.x, new_size.y, Image.INTERPOLATE_BILINEAR)
+		current_screen_scale = os_screen_scale
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
