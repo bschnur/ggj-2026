b23599b0fdb5077280c395b5cdd9ac89221c2d00
Benjamin Schnur <benjamin.schnur@gmail.com>
2026-01-30 23:48:14 -0800

    First go at blending texture colors. Almost there - but colors look washed out / translucent.

--- title_screen.gd
+++ title_screen.gd
@@ -3,9 +3,8 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	%"Mix Viewport".size = get_viewport().get_visible_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+	#pass
