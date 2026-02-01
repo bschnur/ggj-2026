3252a98897ea6d7d7b606803e224a8ff5a540964
Benjamin Schnur <benjamin.schnur@gmail.com>
2026-02-01 15:54:05 -0800

    Fixed the button glitch. Focus texture was transparent but Focus > Mode wasn't set to None.

--- shaders/mix_subviewport.gd
+++ shaders/mix_subviewport.gd
@@ -1,8 +1,21 @@
 extends SubViewport
 
 func _process(_delta: float) -> void:
+	# Force the RenderingServer to treat this viewport as "dirty" every frame.
+	# This ensures the global uniform 'mouse_pos' is always re-processed by shaders.
+	
 	# The mouse position within the 0 to size.x/y range of THIS viewport.
 	# It is the "canonical" coordinate for FRAGCOORD.
 	var local_mouse = get_mouse_position()
 	# Update the mouse position Global Shader Parameter.
 	RenderingServer.global_shader_parameter_set("mouse_pos", local_mouse)
+
+
+func _on_blue_mask_pressed(source: BaseButton) -> void:
+	disable_mask_button(source)
+
+func _on_red_mask_pressed(source: BaseButton) -> void:
+	disable_mask_button(source)
+
+func disable_mask_button(source: BaseButton) -> void:
+	source.set_deferred("disabled", true)
